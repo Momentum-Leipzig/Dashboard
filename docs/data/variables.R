d0b482caf61dcbcb29921f3a87ae1624f9ffaaa1
RichardJanzen <richard_janzen@web.de>
2020-10-06 09:30:22 +0200

    New graphs
    Added new graphs and tried some visualisations of density/distributions

--- docs/data/variables.R
+++ docs/data/variables.R
@@ -3,3 +3,5 @@ data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_8.sav")
 
 HomeOffice = c("ID", "T1_home_office", grep("_HO",names(data), value = T))
 data_HO = data[,HomeOffice]
+
+write.csv(data_HO, file = "data/data_Homeoffice.csv")
