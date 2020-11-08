3b437cac3e0e131b9ca23f6e87fa5b472f07dc15
RichardJanzen <richard_janzen@web.de>
2020-11-08 18:49:00 +0100

    Final Version for Revision
    All the graphs and texts are there, the new measurment wave is included.

--- data/variables.R
+++ data/variables.R
@@ -1,7 +1,7 @@
 library(haven)
-data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_8.sav")
+data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_9.sav")
 
-HomeOffice = c("ID", "T1_home_office", grep("_HO",names(data), value = T))
+HomeOffice = c("ID", "T1_home_office", grep("_HO",names(data), value = T), grep("_covid11",names(data), value = T))
 data_HO = data[,HomeOffice]
 
 write.csv(data_HO, file = "data/data_Homeoffice.csv")
