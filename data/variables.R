cb8ab34d9843173bf285e704d04990e17085b39b
RichardJanzen <richard_janzen@web.de>
2020-11-27 17:01:25 +0100

    Update variables.R

--- data/variables.R
+++ data/variables.R
@@ -1,5 +1,5 @@
 library(haven)
-data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_9.sav")
+data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_10.sav")
 
 HomeOffice = c("ID", "T1_home_office", grep("_HO",names(data), value = T), grep("_covid11",names(data), value = T))
 data_HO = data[,HomeOffice]
