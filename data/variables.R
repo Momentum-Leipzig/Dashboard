cb3d7ee703e0a55dcc21928b7d5385c180d6947d
RichardJanzen <richard_janzen@web.de>
2021-01-15 18:24:35 +0100

    Delete variables.R

--- data/variables.R
+++
@@ -1,7 +0,0 @@
-library(haven)
-data = read_sav("data/Respondi2019_2020_MatchedWaves1_to_11.sav")
-
-HomeOffice = c("ID", "T1_home_office", grep("_HO",names(data), value = T), grep("_covid11",names(data), value = T))
-data_HO = data[,HomeOffice]
-
-write.csv(data_HO, file = "data/data_HomeofficeT11.csv")
